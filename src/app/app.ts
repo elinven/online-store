c24ae8293e1c8111e0ad34d9d080ea5a1ac94195
Kirill <bloodwingest@gmail.com>
2023-01-06 22:11:56 +0300

    fix: revert app changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,21 +2,18 @@ import Component from "../components/component";
 import { Router } from "../router/index";
 import { Header } from "../components/header/index";
 import { Footer } from "../components/footer/index";
-import { Filter } from "../components/filters/filter";
 
 export class App {
   private header;
   private main;
   private router;
   private footer;
-  private filters;
 
   constructor(private rootElement: HTMLElement) {
     this.header = new Header(this.rootElement);
     this.main = new Component(this.rootElement, "main", ["main"]);
     this.router = new Router(this.main.elem); 
     this.footer = new Footer(this.rootElement);
-    this.filters = new Filter(this.main.elem);
   }
 
   start(): void {
