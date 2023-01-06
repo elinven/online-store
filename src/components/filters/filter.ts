348d13d8bda4f8b9996610df96b758298e810ec6
Kirill <bloodwingest@gmail.com>
2023-01-06 19:32:24 +0300

    feat: some work on filters

--- src/components/filters/filter.ts
+++ src/components/filters/filter.ts
@@ -1,5 +1,6 @@
 import Component from "../component";
 import "./style.css";
+/* import cardsInfo from "../goods" */
 
 export class Filter extends Component {
   private categoryFilter;
@@ -8,20 +9,20 @@ export class Filter extends Component {
   private stockFilter;
 
   constructor(parentNode: HTMLElement) {
-    super(parentNode, "div", ["footer"]);
+    super(parentNode, "div", ["filter-wrapper"]);
 
-    this.categoryFilter = new Component(this.elem, "div", ["footer-left"]);
-    this.brandFilter = new Component(this.elem, "div", ["footer-left"]);
-    this.footerLeftContainer = new Component(this.elem, "div", ["footer-left"]);
-    this.footerLeftContainer = new Component(this.elem, "div", ["footer-left"]);
+    this.categoryFilter = new Component(this.elem, "div", ["filter-wrapper-option"]);
+    this.brandFilter = new Component(this.elem, "div", ["filter-wrapper-option"]);
+    this.priceFilter = new Component(this.elem, "div", ["filter-wrapper-option"]);
+    this.stockFilter = new Component(this.elem, "div", ["filter-wrapper-option"]);
 
 
     //this.githubLogo.elem.style.backgroundImage = `url("../../assets/svg/github.svg")`;
-    this.firstTeamDeveloperLink.elem.setAttribute("href", "https://github.com/elinven");
+/*     this.firstTeamDeveloperLink.elem.setAttribute("href", "https://github.com/elinven");
     this.firstTeamDeveloperLink.elem.setAttribute("target", "_blank");
     this.secondTeamDeveloperLink.elem.setAttribute("href", "https://github.com/slysnek");
     this.secondTeamDeveloperLink.elem.setAttribute("target", "_blank");
-    this.rsschool.elem.setAttribute("href", "https://rs.school/js/");
+    this.rsschool.elem.setAttribute("href", "https://rs.school/js/"); */
     //this.footerRightContainer.elem.style.backgroundImage = `url("../../assets/svg/rsshool.svg")`;
   }
 }
