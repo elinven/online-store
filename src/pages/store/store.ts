828d64cbb542246089610a3d3d2622e0ecb7e7dc
Kirill <bloodwingest@gmail.com>
2023-01-07 19:01:15 +0300

    fix: fix filter style

--- src/pages/store/store.ts
+++ src/pages/store/store.ts
@@ -3,13 +3,11 @@ import { Filters } from "../../components/filters/filters"
 import "./store.css";
 
 export class StorePage extends Component {
-  private storeContent;
   private filterWrapper;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["store-page"]);
 
-    this.storeContent = new Component(this.elem, "h1", ["store-content"], "PRODUCTS");
     this.filterWrapper = new Filters(this.elem);
 
   }
