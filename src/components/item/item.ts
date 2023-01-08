db730ec7adb1ca7e7d449a86550844b764ca760f
Kirill <bloodwingest@gmail.com>
2023-01-08 16:44:01 +0300

    feat: add search and sort

--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -47,49 +47,5 @@ export class Item extends Component {
     this.itemButtonDetails = new Component(this.itemButtons.elem, 'button', ['goods-item-button', 'goods-item-details'], 'Details')
     this.itemButtonAdd = new Component(this.itemButtons.elem, 'button', ['goods-item-button', 'goods-item-add'], '+')
 
-
-
-/*     if(filterType === 'range'){
-      const minMax:Array<number> = [];
-      cardsInfo().then((res) => {
-        res.products.forEach((el:Product) => {
-          console.log(el);
-          if(filterName === 'Stock'){
-            minMax.push(el.stock);
-          } else{
-            minMax.push(el.price);
-          }
-        });
-        minMax.sort((a, b) => a - b)
-        this.filterInput = new Component(this.elem, 'input', []);
-        this.filterInput.elem.setAttribute('type', filterType);
-        this.filterInput.elem.setAttribute('min', minMax[0].toString())
-        this.filterInput.elem.setAttribute('max', minMax[minMax.length-1].toString())
-      });
-    } */
-
-/*     if(filterType === 'checkbox'){
-      const categories:Array<string> = [];
-      cardsInfo().then((res) => {
-        res.products.forEach((el:Product) => {
-          if(filterName === 'Categories'){
-            categories.push(el.category)
-          } else {
-            categories.push(el.brand)
-          }
-        });
-        const allCategories = new Set(...[categories]);
-        allCategories.forEach((el) => {
-          console.log(el);
-          this.filterCheckboxWrapper = new Component(this.elem, 'div', ['category-option']);
-          this.filterLabel = new Component(this.filterCheckboxWrapper.elem, 'label', ['category-label'], el)
-          this.filterInput = new Component(this.filterCheckboxWrapper.elem, 'input', []);
-          this.filterLabel.elem.setAttribute ('for', el)
-          this.filterInput.elem.setAttribute('type', filterType);
-          this.filterInput.elem.setAttribute('id', el);
-        })
-      });
-    } */
-   
   }
 }
--- src/pages/store/store.ts
+++ src/pages/store/store.ts
@@ -1,18 +1,26 @@
 import Component from "../../components/component";
 import { Filters } from "../../components/filters/filters"
 import { Items } from "../../components/item/items"
+import { Search } from "../../components/search&sort/search";
+import { Sort } from "../../components/search&sort/sort";
 import "./store.css";
 
 export class StorePage extends Component {
   private filterWrapper;
   private itemsWrapper;
   private goodsWrapper;
+  private searchAndSortWrapper;
+  private search;
+  private sort;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["store-page"]);
 
     this.filterWrapper = new Filters(this.elem);
     this.goodsWrapper = new Component(this.elem, 'div', ['goods-wrapper']);
+    this.searchAndSortWrapper = new Component(this.goodsWrapper.elem, 'div', ['sort-and-search-wrapper']);
+    this.search = new Search(this.searchAndSortWrapper.elem);
+    this.sort = new Sort(this.searchAndSortWrapper.elem);
     this.itemsWrapper = new Items(this.goodsWrapper.elem);
 
   }
