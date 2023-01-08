d3ade7fa42f4b781737187a9ab4b597ab29f76fb
Kirill <bloodwingest@gmail.com>
2023-01-08 16:15:57 +0300

    feat: add wrapper to showcase

--- src/pages/store/store.ts
+++ src/pages/store/store.ts
@@ -6,12 +6,14 @@ import "./store.css";
 export class StorePage extends Component {
   private filterWrapper;
   private itemsWrapper;
+  private goodsWrapper;
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["store-page"]);
 
     this.filterWrapper = new Filters(this.elem);
-    this.itemsWrapper = new Items(this.elem);
+    this.goodsWrapper = new Component(this.elem, 'div', ['goods-wrapper']);
+    this.itemsWrapper = new Items(this.goodsWrapper.elem);
 
   }
 }
