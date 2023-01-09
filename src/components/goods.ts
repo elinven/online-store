323704147d1758e5100339f8d46397749776b361
Kirill <bloodwingest@gmail.com>
2023-01-09 13:07:09 +0300

    feat: add interface for data response

--- src/components/goods.ts
+++ src/components/goods.ts
@@ -1,6 +1,9 @@
+import { IFakeDataResponse } from './../types/index';
+
 export default async function getInfo() {
+  
   const query = await fetch('https://dummyjson.com/products?limit=50');
-  const cardsInfo = await query.json();
+  const cardsInfo: IFakeDataResponse = await query.json();
   if(cardsInfo){
       return cardsInfo;
   } else{
