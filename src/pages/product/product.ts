aa402461601294b0547c9956ec7dedd0ddd56178
Kirill <bloodwingest@gmail.com>
2023-01-09 13:14:22 +0300

    feat: add product details page and rewamp routing

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,13 +1,20 @@
 import Component from "../../components/component";
+import { Model } from "../../model/Model";
 import "./product.css";
 
 export class ProductPage extends Component {
   private productDetails;
 
-  constructor(parentNode: HTMLElement) {
+  constructor(parentNode: HTMLElement, model: Model, id?: number) {
     super(parentNode, "div", ["product-page"]);
+    const state = model.getState();
 
-    this.productDetails = new Component(this.elem, "h1", ["product-details"], "PRODUCT DETAILS");
+    const product = state.products.find((item: { id: number; }) => item.id === id);
 
+    if (product) {
+      this.productDetails = new Component(this.elem, "h1", ["product-details"], JSON.stringify(product, null, 6));
+    } else {
+      this.productDetails = new Component(this.elem, "h1", ["product-details"], "PRODUCT DETAILS with following id were not found:" + id);
+    }
   }
 }
