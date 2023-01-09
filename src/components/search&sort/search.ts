6cd01e46a5af349d55d267347c21ebfb71c77eb1
Kirill <bloodwingest@gmail.com>
2023-01-09 13:14:57 +0300

    feat: add search

--- src/components/search&sort/search.ts
+++ src/components/search&sort/search.ts
@@ -5,10 +5,11 @@ export class Search extends Component {
 
   private searchField: Component | undefined;
 
-  constructor(parentNode: HTMLElement) {
+  constructor(parentNode: HTMLElement, onKeyDown: (event: KeyboardEvent) => void) {
     super(parentNode, "div", ["search-wrapper"]);
 
     this.searchField = new Component(this.elem, 'input', ['goods-search']);
-    this.searchField.elem.setAttribute('placeholder', 'Find item')
+    this.searchField.elem.setAttribute('placeholder', 'Find item');
+    this.searchField.elem.addEventListener('keyup', onKeyDown);
   }
 }
