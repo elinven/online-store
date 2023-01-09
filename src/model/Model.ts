7cdea820b6ee64a6b489142f20c7ec0558b1a198
Kirill <bloodwingest@gmail.com>
2023-01-09 13:06:30 +0300

    feat: add model

---
+++ src/model/Model.ts
@@ -0,0 +1,30 @@
+import { IBaseState, Product, TSubscriber } from "../types/index";
+
+export class Model {
+  private state: IBaseState;
+  private subscribers: TSubscriber[];
+
+  constructor() {
+    this.state = {
+      searchQuery: '',
+      products: [],
+    };
+
+    this.subscribers = [];
+  }
+
+  getState = () => structuredClone(this.state);
+
+  setState = (state: IBaseState) => {
+    this.state = state;
+    this.fire();
+  }
+
+  subscribe = (callback: TSubscriber) => {
+    this.subscribers.push(callback);
+  }
+
+  fire = () => {
+    this.subscribers.forEach((cb) => cb(this.getState()));
+  }
+}
