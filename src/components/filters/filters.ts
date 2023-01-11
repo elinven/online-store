a718e464e3556a1e9b76a4de05bacb476e7a619c
Kirill <bloodwingest@gmail.com>
2023-01-11 09:00:21 +0300

    fix: delete unused component

--- src/components/filters/filters.ts
+++ src/components/filters/filters.ts
@@ -9,7 +9,6 @@ export class Filters extends Component {
 
 
   private filterOption1;
-  private testFilter;
   private filterOption2;
   private filterOption3;
   private filterOption4;
@@ -43,14 +42,7 @@ export class Filters extends Component {
     this.resetFilters = new Component(this.elem, 'button', ['reset-filter-button'], 'Reset Filters')
     this.filterOption1 = new Filter(this.elem, 'range', 'Price', filterCheckbox);
     this.filterOption2 = new Filter(this.elem, 'range', 'Stock', filterCheckbox);
-
-    this.testFilter = new Component(this.elem, 'div', ['test-filter'])
-
     this.filterOption3 = new Filter(this.elem, 'checkbox', 'Brand', filterCheckbox);
     this.filterOption4 = new Filter(this.elem, 'checkbox', 'Categories', filterCheckbox);
-   
-
-    let testFilterDiv = document.querySelector('.test-filter')
-
   }
 }
