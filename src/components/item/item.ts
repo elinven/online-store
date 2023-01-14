03d04a1c899865b210682e0889927ca990efd082
elinven <ivenhea@gmail.com>
2023-01-14 21:39:12 +0300

    feat: add adding to cart for store item

--- src/components/item/item.ts
+++ src/components/item/item.ts
@@ -1,3 +1,4 @@
+import { Product, ProductCart } from "../../types/index";
 import Component from "../component";
 import "./style.css";
 
@@ -18,38 +19,64 @@ export class Item extends Component {
   private itemDiscount: Component | undefined;
   private itemRating: Component | undefined;
   private itemStock: Component | undefined;
-  private itemButtonAdd: Component | undefined;
+  private itemButtonAdd: Component;
   private itemButtonDetails: Component | undefined;
 
-  constructor(parentNode: HTMLElement, itemName: string, itemBrand: string,
-     itemCategory: string,  itemImages:string, itemPrice:string, itemDiscount:string, itemRating:string, itemStock: string, id: number) {
+  constructor(parentNode: HTMLElement, goodCart: ProductCart, goodData: Product) {//itemName: string, itemBrand: string,
+     //itemCategory: string,  itemImages:string, itemPrice:string, itemDiscount:string, itemRating:string, itemStock: string, id: number) {
     super(parentNode, "div", ["goods-item"]);
 
     this.itemUpper = new Component(this.elem, 'div', ['goods-item-upper-section'])
-    this.itemName = new Component(this.itemUpper.elem, 'h3', ['goods-item-name'], itemName)
+    this.itemName = new Component(this.itemUpper.elem, 'h3', ['goods-item-name'], goodData.title)
     this.itemCatAndBrandWrap = new Component(this.itemUpper.elem, 'div', ['goods-item-category-and-brand-wrapper'])
-    this.itemBrand = new Component(this.itemCatAndBrandWrap.elem, 'div', ['goods-item-info-brand'], itemBrand)
-    this.itemCategory = new Component(this.itemCatAndBrandWrap.elem, 'div', ['goods-item-info-category'], itemCategory)
+    this.itemBrand = new Component(this.itemCatAndBrandWrap.elem, 'div', ['goods-item-info-brand'], goodData.brand)
+    this.itemCategory = new Component(this.itemCatAndBrandWrap.elem, 'div', ['goods-item-info-category'], goodData.category)
 
     this.itemPhoto = new Component(this.elem, 'div', ['goods-item-photo-wrapper'])
-    this.itemPhoto.elem.style.backgroundImage = `url(${itemImages})`;
+    this.itemPhoto.elem.style.backgroundImage = `url(${goodData.thumbnail})`;
 
     this.itemPriceInfo = new Component(this.elem, 'div', ['goods-item-info'])
-    this.itemPrice = new Component(this.itemPriceInfo.elem, 'div', ['goods-item-info-price'], `${itemPrice}$`)
-    this.itemDiscount = new Component(this.itemPriceInfo.elem, 'div', ['goods-item-info-discount'], `(-${itemDiscount}%)`)
+    this.itemPrice = new Component(this.itemPriceInfo.elem, 'div', ['goods-item-info-price'], `${goodData.price}$`)
+    this.itemDiscount = new Component(this.itemPriceInfo.elem, 'div', ['goods-item-info-discount'], `(-${goodData.discountPercentage}%)`)
 
     this.itemLower = new Component(this.elem, 'div', ['goods-item-lower-section'])
     this.itemRatingAndStockWrap = new Component(this.itemLower.elem, 'div', ['goods-item-rating-and-stock'])
-    this.itemRating = new Component(this.itemRatingAndStockWrap.elem, 'div', ['goods-item-rating'], `${itemRating}★`)
-    this.itemStock = new Component(this.itemRatingAndStockWrap.elem, 'div', ['goods-item-stock'], `${itemStock} left`)
+    this.itemRating = new Component(this.itemRatingAndStockWrap.elem, 'div', ['goods-item-rating'], `${goodData.rating}★`)
+    this.itemStock = new Component(this.itemRatingAndStockWrap.elem, 'div', ['goods-item-stock'], `${goodData.stock} left`)
 
     this.itemButtons = new Component(this.itemLower.elem, 'div', ['goods-item-buttons'])
     this.itemButtonDetails = new Component(this.itemButtons.elem, 'button', ['goods-item-button', 'goods-item-details'], 'Details')
-    this.itemButtonAdd = new Component(this.itemButtons.elem, 'button', ['goods-item-button', 'goods-item-add'], '+')
+    this.itemButtonAdd = new Component(this.itemButtons.elem, 'button', ['goods-item-button', 'goods-item-add'], goodCart.goods.some((e) => e.product.id === goodData.id) ? "-" : "+")
 
     this.itemButtonDetails.elem.addEventListener("click", () => {
-      window.location.hash = `#/product-details/${id}`;
+      window.location.hash = `#/product-details/${goodData.id}`;
     });
+
+    this.itemButtonAdd.elem.onclick = () => {
+      if (!goodCart.goods.some((e) => e.product.id === goodData.id)) {
+        goodCart.goods.push({product: goodData, amount: 1});
+        goodCart.amount ++;
+        goodCart.summa += goodData.price;
+        this.itemButtonAdd.elem.textContent = "-";
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('.cart-summa')!.textContent = `$${goodCart.summa.toFixed(2)}`;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('.cart-amount')!.textContent = `${goodCart.amount}`;
+        localStorage.setItem('cart', JSON.stringify(goodCart));
+      } else {
+        const dropIndex = goodCart.goods.findIndex((e) => e.product.id === goodData.id);
+        goodCart.amount -= goodCart.goods[dropIndex].amount;
+        goodCart.summa -= goodCart.goods[dropIndex].product.price * goodCart.goods[dropIndex].amount;
+        goodCart.goods.splice(dropIndex, 1);
+        this.itemButtonAdd.elem.textContent = "+";
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('.cart-summa')!.textContent = `$${goodCart.summa.toFixed(2)}`;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('.cart-amount')!.textContent = `${goodCart.amount}`;
+        localStorage.setItem('cart', JSON.stringify(goodCart));
+        console.log(goodCart);
+      }
+    }
 
   }
 }
